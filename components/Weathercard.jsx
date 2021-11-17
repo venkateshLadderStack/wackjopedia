130a31f61e3a7a37e0d8593418e2048930227084
karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2021-11-17 13:26:18 +0530

    frame5 page added

--- components/Weathercard.jsx
+++ components/Weathercard.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import weatherStyle from "../styles/css/weathercard.module.css";
 
-function Weathercard() {
+function Weathercard({ detail }) {
   return (
     <>
-      <div className={weatherStyle.system__single__box}>
+      <div
+        className={`${weatherStyle.system__single__box} 
+        ${detail.month == "Styczeń" && weatherStyle.jan}
+        ${detail.month == "Luty" && weatherStyle.feb}
+        ${detail.month == "Marzec" && weatherStyle.mar}
+        ${detail.month == "Kwiecień" && weatherStyle.april}
+        ${detail.month == "Może" && weatherStyle.may}
+        ${detail.month == "Czerwiec" && weatherStyle.jun}
+        ${detail.month == "Lipiec" && weatherStyle.july}
+        ${detail.month == "Sierpień" && weatherStyle.aug}
+        ${detail.month == "Wrzesień" && weatherStyle.sep}
+        ${detail.month == "Październik" && weatherStyle.oct}
+        ${detail.month == "Listopad" && weatherStyle.nov}
+        ${detail.month == "Grudzień" && weatherStyle.dec}
+        `}
+      >
         <div className={weatherStyle.system__fx}>
           <div className={weatherStyle.system__1st}>
-            <h6>Styczeń</h6>
+            <h6>{detail.month}</h6>
             <a href="#">idealna pogoda</a>
           </div>
           <div className={weatherStyle.system__2}>
--- pages/frame5.jsx
+++ pages/frame5.jsx
@@ -72,6 +72,45 @@ const pillsList = [
   },
 ];
 
+const MonthlyWeather = [
+  {
+    month: "Styczeń",
+  },
+  {
+    month: "Luty",
+  },
+  {
+    month: "Marzec",
+  },
+  {
+    month: "Kwiecień",
+  },
+  {
+    month: "Może",
+  },
+  {
+    month: "Czerwiec",
+  },
+  {
+    month: "Lipiec",
+  },
+  {
+    month: "Sierpień",
+  },
+  {
+    month: "Wrzesień",
+  },
+  {
+    month: "Październik",
+  },
+  {
+    month: "Listopad",
+  },
+  {
+    month: "Grudzień",
+  },
+];
+
 function frame5() {
   return (
     <>
@@ -110,18 +149,9 @@ function frame5() {
                   desc={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. `}
                   highlighted={`It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}
                 />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
-                <Weathercard />
+                {MonthlyWeather.map((item, index) => (
+                  <Weathercard key={index} detail={item} />
+                ))}
                 {details.map((detail, index) => (
                   <Frame26Text
                     key={index}
