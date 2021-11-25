12826267ebceb2177245a8120b0702c9fd66ab8a
karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2021-11-25 19:01:45 +0530

    banner added

--- components/banner.jsx
+++ components/banner.jsx
@@ -1,13 +1,42 @@
 import Image from "next/image";
 import bannerImg from "../public/img/banner.png";
 import ImageComponent from "./ImageComponent";
+import Link from "next/link";
+import styles from "../styles/css/banner.module.css";
 
 const Banner = ({ title, image }) => {
   return (
     <>
       <h3 className="py-4">{title}</h3>
-      <div className="banner">
-        {image ? (
+      <div className={styles.banner}>
+        <div className={styles.bgImg}>
+          <div className={styles.caption}>
+            <h3>hello</h3>
+            <h3>Second Line</h3>
+            <div className={styles.check}>
+              <i
+                className="fa fa-check"
+                style={{ color: "orange" }}
+                aria-hidden="true"
+              >
+                <span style={{ color: "white" }} className="px-2">
+                  Kiedy chcesz jechać
+                </span>
+              </i>
+              <i
+                className="fa fa-check"
+                style={{ color: "orange" }}
+                aria-hidden="true"
+              >
+                <span style={{ color: "white" }} className="px-3">
+                  Jaka pogoda Cię interesuje
+                </span>
+              </i>
+            </div>
+            <button className={styles.btn}>sprawdź oferty</button>
+          </div>
+        </div>
+        {/* {image ? (
           <ImageComponent
             layout="responsive"
             loading="lazy"
@@ -25,7 +54,7 @@ const Banner = ({ title, image }) => {
             src={bannerImg}
             alt="banner"
           />
-        )}
+        )} */}
       </div>
     </>
   );
